a6fb478d29228df9716f36f57bca98427c4a45be
moreyrathomas14-maker <moreyrathomas14@gmail.com>
2026-03-03 20:40:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// === VISUALIZADOR DE MUEBLES ===
-function cambiarTela(tipo) {
-    const imagenMueble = document.getElementById('mueble-base');
-    const linkWA = document.getElementById('whatsapp-link');
-    
-    // Tu número real
-    const miTelefono = "5493525649575"; 
-
-    const catalogoTelas = {
-        'pana-azul': { img: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?auto=format&fit=crop&w=800&q=80', nombre: 'Pana Azul' },
-        'pana-roja': { img: 'https://images.unsplash.com/photo-1583847268964-b28dc2f51ac9?auto=format&fit=crop&w=800&q=80', nombre: 'Pana Violeta' },
-        'ecocuero': { img: 'https://images.unsplash.com/photo-1567016432779-094069958ad5?auto=format&fit=crop&w=800&q=80', nombre: 'Verde Bosque' },
-        'lino': { img: 'https://images.unsplash.com/photo-1540574163026-643ea20ade25?auto=format&fit=crop&w=800&q=80', nombre: 'Ocre' }
-    };
-
-    imagenMueble.style.opacity = 0;
-    
-    setTimeout(() => {
-        imagenMueble.src = catalogoTelas[tipo].img;
-        imagenMueble.style.opacity = 1;
-        
-        const mensaje = `Hola! Estuve viendo la web de GM Tapizados y me interesa un presupuesto para el color: ${catalogoTelas[tipo].nombre}`;
-        const mensajeCodificado = encodeURIComponent(mensaje);
-        
-        linkWA.href = `https://wa.me/${miTelefono}?text=${mensajeCodificado}`;
-    }, 300);
-}
-
 // === ACORDEÓN DE MATERIALES ===
 function toggleMaterial(elementoTarjeta) {
     elementoTarjeta.classList.toggle('activo');
